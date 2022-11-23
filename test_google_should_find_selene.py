940bdfce6df334fd32458cdf2ab5b0f5c41e3a26
DmanYV <dima-xxx@mail.ru>
2022-11-23 23:51:54 +0300

    - fixed the style

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -6,6 +6,7 @@ def test_google_search(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
+
 def test_google_search_bad_request(open_browser):
     browser.element('[name="q"]').should(be.blank).type('sdfNOSDNgiouBSDoignlkjjdosgn').press_enter()
     browser.element('#res').should(have.text('По запросу sdfNOSDNgiouBSDoignlkjjdosgn ничего не найдено.'))
