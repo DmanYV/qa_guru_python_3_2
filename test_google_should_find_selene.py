8b2e68bd883438c689e4e302018d323c03e02089
DmanYV <dima-xxx@mail.ru>
2022-11-23 17:19:44 +0300

    - fixed test_google_should_find_selene

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -4,9 +4,8 @@ from selene import be, have
 
 def test_google_search(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 def test_google_search_bad_request(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.no.text('Домашнее задание'))
-
+    browser.element('[name="q"]').should(be.blank).type('sdfNOSDNgiouBSDoignlkjjdosgn').press_enter()
+    browser.element('#res').should(have.text('По запросу sdfNOSDNgiouBSDoignlkjjdosgn ничего не найдено.'))
